c2b069a7aad2466b145563e1c903327ad1933e6e
Srinath Shiv Kumar <kumar256@purdue.edu>
2017-09-29 11:45:41 -0400

    Add files via upload

--- img2obj.py
+++ img2obj.py
@@ -9,7 +9,7 @@ from torch.autograd import Variable
 import matplotlib.image as img
 from PIL import Image
 import numpy as np
-# import cv2
+import cv2
 import time
 
 # Training settings
@@ -18,7 +18,7 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=1, metavar='N',
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.0005, metavar='LR',
                     help='learning rate (default: 0.0001)')
@@ -117,30 +117,81 @@ class Net(nn.Module):
             test_loss, correct, len(test_loader.dataset),
             100. * correct / len(test_loader.dataset)))
 
+    def view(self, input_image):
+        # ===== Put text =========
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (10, 500)
+        fontScale = 1
+        fontColor = (255, 255, 255)
+        lineType = 2
+
+        input_image_tensor = torch.from_numpy(input_image)
+        input_image_tensor = Variable(input_image_tensor)
+        input_image_tensor = input_image_tensor.view(1, 3, 32, 32)
+        pred = model.forward(input_image_tensor)
+        pred = pred.data.numpy()
+        pred_label = np.argmax(pred, axis=1)
+
+        resized_input_image = cv2.resize(input_image, (1024, 786))
+
+        cv2.putText(resized_input_image, str(pred_label),
+                    bottomLeftCornerOfText,
+                    font,
+                    fontScale,
+                    fontColor,
+                    lineType)
+
+        cv2.imshow('frame', resized_input_image)
+
+    def cam(self, idx=0):
+        camera_port = idx
+        camera = cv2.VideoCapture(camera_port)
+        print("Taking images...")
+        t_end = time.time() + 60 * 0.2
+
+        while time.time() < t_end:
+            retval, camera_capture = camera.read()
+            resized_image = cv2.resize(camera_capture, (32, 32))
+
+            cv2.imwrite('frame.png', resized_image)
+
+            input_image = img.imread("frame.png")
+            self.view(input_image)
+
+        del(camera)
+        print("Camera deleted...")
+
 
 model = Net()
 if args.cuda:
     model.cuda()
-
-optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
-# model.train()
-# torch.save(model.state_dict(), 'save_model.pt')
-
 model.load_state_dict(torch.load('save_model.pt'))
+model.cam(0)
 
-# model.test()
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# model = Net()
+# if args.cuda:
+#     model.cuda()
+
+# # optimizer = optim.Adam(model.parameters(), lr=args.lr)
+# # model.train()
+# # torch.save(model.state_dict(), 'save_model.pt')
+# model.load_state_dict(torch.load('save_model.pt'))
+
+# # model.test()
 # # Camera part
 # # Camera 0 is the integrated web cam on my netbook
+
+
 # camera_port = 0
 
 # # Now we can initialize the camera capture object with the cv2.VideoCapture class.
 # # All it needs is the index to a camera port.
 # camera = cv2.VideoCapture(camera_port)
 
-# Captures a single image from the camera and returns it in PIL format
+# # Captures a single image from the camera and returns it in PIL format
 
 
 # def get_image():
@@ -149,34 +200,47 @@ model.load_state_dict(torch.load('save_model.pt'))
 #     return im
 
 
+# # ===== Put text =========
+# font = cv2.FONT_HERSHEY_SIMPLEX
+# bottomLeftCornerOfText = (10, 500)
+# fontScale = 1
+# fontColor = (255, 255, 255)
+# lineType = 2
+
+
 # print("Taking images...")
 
 # t_end = time.time() + 60 * 0.2
 
 # while time.time() < t_end:
 #     camera_capture = get_image()
-#     resized_image = camera_capture.resize((32, 32))
-#     cv2.imshow('frame', camera_capture)
-#     input_image = resized_image
-#     # input_image = torch.from_numpy(input_image)
+#     resized_image = cv2.resize(camera_capture, (32, 32))
+
+#     cv2.imwrite('frame.png', resized_image)
+
+#     input_image = img.imread("frame.png")
+#     input_image = torch.from_numpy(input_image)
 #     input_image = Variable(input_image)
-#     # input_image = input_image.view(1, 3, 32, 32)
+#     input_image = input_image.view(1, 3, 32, 32)
 #     pred = model.forward(input_image)
 #     pred = pred.data.numpy()
 #     pred_label = np.argmax(pred, axis=1)
-#     print(pred_label)
+
+#     # print(pred_label)
+
+#     cv2.putText(camera_capture, str(pred_label),
+#                 bottomLeftCornerOfText,
+#                 font,
+#                 fontScale,
+#                 fontColor,
+#                 lineType)
+
+#     cv2.imshow('frame', camera_capture)
 
 
-# You'll want to release the camera, otherwise you won't be able to create a new
-# capture object until your script exits
+# # You'll want to release the camera, otherwise you won't be able to create a new
+# # capture object until your script exits
 # del(camera)
 # print("Camera deleted...")
 
-input_image = img.imread("automobile10.png")
-input_image = torch.from_numpy(input_image)
-input_image = Variable(input_image)
-input_image = input_image.view(1, 3, 32, 32)
-pred = model.forward(input_image)
-pred = pred.data.numpy()
-pred_label = np.argmax(pred, axis=1)
-print(pred_label)
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
